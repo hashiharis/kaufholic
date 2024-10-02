35fb22237c2bba8bd6a189263698a0cf0b20c287
hashiharis <100787257+hashiharis@users.noreply.github.com>
2024-10-02 12:43:39 +0530

    added styles for seller sign in component, responsiveness,minor updations in responsiveness for seller sign up component

--- client/wishit-shopit-app/src/App.jsx
+++ client/wishit-shopit-app/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
+import { SignIn } from './components/seller/signin/SignIn'
 import { SignUp } from './components/seller/signup/SignUp'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-    <SignUp/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/seller/signup' element={<SignUp/>}/>
+      <Route path='/seller/signin' element={<SignIn/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/wishit-shopit-app/src/components/seller/signin/SignIn.jsx
@@ -0,0 +1,63 @@
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import styles from "./signin.module.css";
+import hidePassIcon from "../../../assets/svg/hidePassIcon.svg";
+import googleIcon from '../../../assets/svg/googleLogo.svg'
+export const SignIn = () => {
+  return (
+    <div className={styles.signInContainer}>
+      <div className={styles.imgSection}></div>
+      <div className={styles.signinSection}>
+        <Form className={styles.form}>
+        <header className={styles.signinHeader}>Sign In </header>
+
+        <Form.Group>
+          <Form.Label className={styles.label}>
+            Email <sup>*</sup>
+          </Form.Label>
+          <InputGroup  className={styles.input}>
+            <Form.Control
+              type="text"
+              placeholder="Enter name"
+              className={styles.formInput}
+            />
+          </InputGroup>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label className={styles.label}>
+            Password <sup>*</sup>
+          </Form.Label>
+          <InputGroup className={styles.input}>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              className={styles.formInput}
+            />
+             <InputGroup.Text>
+                <img
+                  src={hidePassIcon}
+                  alt="icon-svg"
+                  className={styles.iconImg}
+                />
+              </InputGroup.Text>
+          </InputGroup>
+        </Form.Group>
+        <div className={styles.forgotPassword}>
+          <a href="">Forgot Password</a>
+        </div>
+        <Button className={`${styles.signInBtn} ${styles.colorSignIn}`}>Sign In</Button>
+        <div className={styles.divider}>OR</div>
+        <Button className={`${styles.signInBtn} ${styles.googleSignIn}`}>
+        <img
+              src={googleIcon}
+              alt="icon-svg"
+              className={styles.googleIcon}
+            />
+          Sign in by Google</Button>
+        </Form>
+        
+      </div>
+    </div>
+  );
+};
