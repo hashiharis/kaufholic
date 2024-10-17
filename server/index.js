b91b9dc8391677a0be9961aec06d86c4f191d62c
hashiharis <100787257+hashiharis@users.noreply.github.com>
2024-10-17 13:33:55 +0530

    added basc config files for backend

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+
+const dotenv=require('dotenv');
+dotenv.config();
+
+
+const PORT=process.env.PORT||3000;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`)
+})
